aeef6c4598be2898da78efc5d0972074dd768d2d
hoangnhps11609 <89326633+hoangnhps11609@users.noreply.github.com>
2022-01-28 17:34:35 +0700

    Update cart-index.py

--- saleapp/cart-index.py
+++ saleapp/cart-index.py
@@ -6,6 +6,8 @@ import utils
 
 app = Flask(__name__)
 
+
+#get all item in cart
 @app.route("/cart", methods=["GET"])
 def get_cart():
     rows = utils.get_Item("Select * from cartItem")
@@ -20,17 +22,21 @@ def get_cart():
         })
     return jsonify({"cartItem": data})
 
+#add product to cart
 @app.route("/cartItem/<product_id>/<int:quantity>", methods=["POST"])
 def add_product(product_id, quantity):
     n = random.randint(0, 9999)
     utils.add_Item_in_Cart(random, product_id, quantity)
     return get_cart()
 
+
+#update item
 @app.route("/cartItem/update/<id>/<int:quantity>", methods=["PUT"])
 def updateQuantity(id, quantity):
     utils.update_Item_in_Cart(id, quantity)
     return get_cart()
 
+#delete item
 @app.route("/cartItem/delete/<id>", methods=["DELETE"])
 def deleteItem(id):
     utils.delete_Item_in_Cart(id)
